cbaec86ebd27d215fb55a4c665d2fca53268a23c
Ansh Bhanushali <bhanusad@mail.uc.edu>
2025-04-22 22:44:02 -0400

    created logger function

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,3 +1,19 @@
 import logging
 import structlog
 from app.config import settings
+
+def configure_logging():
+    logging.basicConfig(
+        format="%(message)s",
+        level=getattr(logging, settings.LOG_LEVEL.upper(), logging.INFO),
+    )
+    structlog.configure(
+        wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
+        processors=[
+            structlog.processors.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.JSONRenderer(),
+        ],
+    )
+
+logger = structlog.get_logger()
