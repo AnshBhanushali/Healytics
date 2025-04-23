8d7e59d45e01ed5696719878a709786c406485a5
Ansh Bhanushali <bhanusad@mail.uc.edu>
2025-04-22 23:28:20 -0400

    created post for text-image genration

--- backend/app/routers/text_image.py
+++ backend/app/routers/text_image.py
@@ -2,3 +2,11 @@ from fastapi import APIRouter, UploadFile, File, HTTPException, status
 from .__init__ import router 
 from app.model import predict_text_image
 from app.logging_conf import logger
+
+router = APIRouter(prefix="/predict", tags=["predict"])
+
+@router.post(
+    "/text-image",
+    response_model=None, 
+    response_model_exclude_none=True
+)
