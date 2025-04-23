34c77d6867e4261b54fc7210a6b4416bf0113436
Ansh Bhanushali <bhanusad@mail.uc.edu>
2025-04-22 22:02:01 -0400

    processing data model

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,3 +8,13 @@ from xgboost import XGBClassifier
 from sklearn.metrics import roc_auc_score, f1_score
 import joblib
 
+# Loading data for classification
+df = pd.read_csv("Disease_symptom_and_patient_profile_dataset.csv")
+
+# Defining features for classification and prediction
+FEATURE_COLS = [
+    "Fever", "Cough", "Fatigue", "Difficulty Breathing",
+    "Age", "Gender", "Blood Pressure", "Cholesterol Level"
+]
+LABEL_COL = "Outcome Variable"
+
