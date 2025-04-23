f65d6d7224719542c9a25871d328ca1f7055b0f0
Ansh Bhanushali <bhanusad@mail.uc.edu>
2025-04-22 22:46:42 -0400

    created loading model functions

--- backend/app/model.py
+++ backend/app/model.py
@@ -2,3 +2,11 @@ import joblib
 import pandas as pd
 from sklearn.pipeline import Pipeline
 from app.schemas import FormInput, PredictionResponse
+
+_model: Pipeline | None = None
+
+def load_model(path: str) -> Pipeline:
+    global _model
+    if _model is None:
+        _model = joblib.load(path)
+    return _model
