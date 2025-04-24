4d8d72ef74bda077f50fe8ec23cdee660e79446d
Ansh Bhanushali <bhanusad@mail.uc.edu>
2025-04-24 13:34:31 -0400

    deployed on GCP

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,6 +1,19 @@
 // utils/api.ts
-import axios from "axios";
+import axios, { AxiosInstance } from 'axios';
 
-export default axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+const defaultBaseURL =
+  typeof window !== 'undefined'
+    ? window.location.origin
+    : '';
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || defaultBaseURL;
+
+// <-- add this
+console.log('[utils/api] ➜ using baseURL:', API_BASE_URL);
+
+const api: AxiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: { Accept: 'application/json' },
 });
+
+export default api;
